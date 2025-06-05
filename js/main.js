315e5ee4722eef05da2349857bdcba4212c55757
Federico Aieta <maxe1111@hotmail.com>
2025-06-05 17:34:49 -0300

    [feature] Logo de navbar cambiado a imagen completa

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ function createCardStructure(product) {
     cardDiv.innerHTML = `
             <div class="card-content">
                 <div class="card-image">
-                    <img src="${product.img}" alt="${product.title}">
+                    <img draggable="false" src="${product.img}" alt="${product.title}">
                 </div>
                 <div class="card-text">
                     <p class="title">${product.title}</p>
